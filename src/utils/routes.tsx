5c585973c88f9425c48dc8f9f85c0bd3f9386627
Andrewkizito <Andrewkizito54@gmail.com>
2023-02-27 08:30:34 +0300

    Removed Unused Import

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -8,34 +8,34 @@ import Cart from 'src/components/pages/client/Cart/Root'
 // Importing admin pages
 import Login from 'src/components/pages/admin/Login/Login'
 import AdminRoot from 'src/components/pages/admin/Root'
-import Dashboard from 'src/components/pages/admin/Dashboard/Dashboard'
 import Products from 'src/components/pages/admin/Products/Products'
+import Orders from 'src/components/pages/admin/Orders/Orders'
 
 export interface AppRoute {
-  title: string
-  path: string
-  component: ReactElement
-  isAdminRoute: boolean
+  title: string;
+  path: string;
+  component: ReactElement;
+  isAdminRoute: boolean;
   nested?: Array<{
-    title: string
-    path: string
-    component: ReactElement
-  }>
+    title: string;
+    path: string;
+    component: ReactElement;
+  }>;
 }
 
-function generateRoutes (isAuthenticated: boolean = false): AppRoute[] {
+function generateRoutes(isAuthenticated = false): AppRoute[] {
   const routes: AppRoute[] = [
     {
       title: 'Shop',
       path: '/',
       component: <Home />,
-      isAdminRoute: false
+      isAdminRoute: false,
     },
     {
       title: 'Cart',
       path: '/cart',
       component: <Cart />,
-      isAdminRoute: false
+      isAdminRoute: false,
     },
     {
       title: 'Admin',
@@ -47,28 +47,23 @@ function generateRoutes (isAuthenticated: boolean = false): AppRoute[] {
             {
               title: 'Login',
               path: '',
-              component: <Login />
-            }
+              component: <Login />,
+            },
           ]
         : [
             {
-              title: 'Dashboard',
-              path: '',
-              component: <Dashboard />
-            },
-            {
               title: 'Products',
-              path: 'products',
-              component: <Products />
+              path: '',
+              component: <Products />,
             },
 
             {
               title: 'Orders',
               path: 'orders',
-              component: <Dashboard />
-            }
-          ]
-    }
+              component: <Orders />,
+            },
+          ],
+    },
   ]
   return routes
 }
